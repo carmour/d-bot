82528f0fcf9f9a7b336594a83fc424f5c1994e42
carmour <callum.m.armour@gmail.com>
2021-02-22 23:45:26 -0800

    automated refreshing tokens, writing to json file

--- index.py
+++ index.py
@@ -2,33 +2,50 @@ from flask import Flask, request, jsonify, make_response
 import os
 from dotenv import load_dotenv
 
-app = Flask(__name__)
+import refresh_tokens
 
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-    if request.method == 'GET':
-        # initiates webhook -- perform CURL request to hit this endpoint
-        verify_token = os.getenv('INITIATE_TOKEN')
-        mode = request.args['hub.mode']
-        token = request.args['hub.verify_token']
-        challenge = request.args['hub.challenge']
-        if (mode and token):
-            if mode == 'subscribe' and token == verify_token:
-                print('webhook verified!')
-                response = jsonify({
-                    'hub.challenge': challenge
-                })
-                return make_response(response), 200
-            else:
-                return make_response('Forbidden'), 403
-    elif request.method == 'POST':
-        # webhook received here
-        # print('request: ', request)
-        print('request.json: ', request.json)
-        print('activity id: ', request.json['object_id'])
-        return make_response('Ok'), 200
+# app = Flask(__name__)
 
-    return 'hello'
+# @app.route('/webhook', methods=['GET', 'POST'])
+# def webhook():
+#     if request.method == 'GET':
+#         # initiates webhook -- perform CURL request to hit this endpoint
+#         verify_token = os.getenv('INITIATE_TOKEN')
+#         mode = request.args['hub.mode']
+#         token = request.args['hub.verify_token']
+#         challenge = request.args['hub.challenge']
+#         if (mode and token):
+#             if mode == 'subscribe' and token == verify_token:
+#                 print('webhook verified!')
+#                 response = jsonify({
+#                     'hub.challenge': challenge
+#                 })
+#                 return make_response(response), 200
+#             else:
+#                 return make_response('Forbidden'), 403
+#     elif request.method == 'POST':
+#         # webhook received here
+#         print('request.json: ', request.json)
+#         # print('activity id: ', request.json['object_id'])
+#         new_activity = request.json
+#         # token refresh strava api
+#         # token write json file
+#         # token read json file
+#         # get request strava api
+#         # give data to webhook function
 
-if __name__ == "__main__":
-    app.run()
+#         return make_response('Ok'), 200
+
+#     return 'hello'
+
+def testing_refresh(user_id):
+    refresh_token = refresh_tokens.find_refresh(user_id)
+    access_token = refresh_tokens.refresh_access(refresh_token[0], refresh_token[1])
+    print(access_token)
+
+testing_refresh('58937648')
+
+# TODO: automate get request to strava api using the generated tokens
+
+# if __name__ == '__main__':
+#     app.run()
---
+++ refresh_tokens.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+# disables unverified https request warning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+load_dotenv()
+
+def find_user(user_id):
+    f = open('user_ids_test_2.json')
+    user_data = json.load(f)
+    for user in user_data:
+        if user['id'] == user_id:
+            return user
+
+def find_refresh(user_id):
+    user = find_user(user_id)
+    return user['refresh_token'], user_id
+
+def write_tokens(new_refresh_token, new_access_token, user_id):
+    user = find_user(user_id)
+    # print(user)
+    with open('user_ids_test_2.json', 'r') as f:
+        user_data = json.load(f)
+        for item in user_data:
+            if item == user:
+                item['access_token'] = new_access_token
+                item['refresh_token'] = new_refresh_token
+    with open('user_ids_test_2.json', 'w') as f:
+        json.dump(user_data, f, indent=4)
+
+# def write_tokens(new_refresh_token, new_access_token, user_id):
+#     user = find_user(user_id)
+#     with open('user_ids_test.json', 'r') as f:
+#         user_data = json.load(f)
+#         for item in user_data:
+#             if item == user:
+#                 item['access_token'] = new_access_token
+#                 item['refresh_token'] = new_refresh_token
+#     with open('user_ids_test.json', 'w') as f:
+#         json.dump(user_data, f, indent=4)
+
+# refreshes access and refresh tokens, given user's refresh token
+def refresh_access(refresh_token, user_id):
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    auth_url = "https://www.strava.com/oauth/token"
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token',
+        'f': 'json'
+    }
+    generate_tokens = requests.post(auth_url, data=payload, verify=False)
+    new_refresh_token = generate_tokens.json()['refresh_token']
+    new_access_token = generate_tokens.json()['access_token']
+    write_tokens(new_refresh_token, new_access_token, user_id)
+    return new_refresh_token, new_access_token
