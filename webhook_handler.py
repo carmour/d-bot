ca5970a1e0ad934ccf1dab40ac13423c5e45a5dc
carmour <callum.m.armour@gmail.com>
2021-03-01 21:12:11 -0800

    first

--- webhook_handler.py
+++ webhook_handler.py
@@ -3,5 +3,4 @@ import get_activity
 
 def testing_refresh(user_id, activity_id):
     access_token = refresh_tokens.return_access_token(user_id)
-    # print(access_token)
     return get_activity.get_activity(activity_id, access_token)
